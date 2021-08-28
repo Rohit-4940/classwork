38077fdfaad08d460b9bb126162f5f099390cb20
Rohit Chapagain <chapagainrohit4940@gmail.com>
2021-08-28 14:41:49 +0545

    unit testing

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Unit testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Unit testing")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f6a5ba2d-485a-4ded-844a-c09577aec4e9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using Component1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Unit_testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void isShape_equalCircle()
+        {
+            var shapeFactorydef = new ShapeFactoryDef();
+            bool result = shapeFactorydef.isCircle("circle");
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void isShape_equalRectangle()
+        {
+            var shapeFactorydef = new ShapeFactoryDef();
+            bool result = shapeFactorydef.isRectangle("rectangle");
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void isShape_equalTringle()
+        {
+            var shapeFactorydef = new ShapeFactoryDef();
+            bool result = shapeFactorydef.isTringle("tringle");
+            Assert.AreEqual(true, result);
+        }
+    }
+}
